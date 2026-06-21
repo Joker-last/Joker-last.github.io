8f55d2a2d7868565ee4a0d8faebb6bd2ba073ca4
Joker-last <2084643886@qq.com>
2026-06-21 16:22:05 +0800

    完成GitHub Pages自动部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.siteURL,
+	site: "https://Joker-last.github.io",
 	base: "/",
 	trailingSlash: "always",
 
